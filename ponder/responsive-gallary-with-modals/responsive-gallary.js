e0b46c13e377949081ba0ed2dc508df072b6dc5e
STCrosland <spencertcrosland@gmail.com>
2026-01-30 15:45:25 -0700

    Gallary
    Gallary assignment completeed and thumbails placed on home and ponder pages

--- ponder/responsive-gallary-with-modals/responsive-gallary.js
+++ ponder/responsive-gallary-with-modals/responsive-gallary.js
@@ -7,10 +7,20 @@ const closeButton = modal.querySelector('.close-viewer');
 // Event listener for opening the modal
 gallery.addEventListener('click', openModal);
 
+// Code to show modal  - Use event parameter 'e'
+
 function openModal(e) {
-    
-// Code to show modal  - Use event parameter 'e'   
-    
+    console.log(e.target);
+
+    const img = e.target
+    const src = img.getAttribute('src');
+    const alt= img.getAttribute('alt');
+    const full = src.replace('sm','full');
+
+    modalImage.src = full;
+    modalImage.alt = alt;
+
+    modal.showModal();
 }
 // Close modal on button click
 closeButton.addEventListener('click', () => {
